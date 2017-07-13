a271eca84c7d06ae35fac2d73438a0ad026ed258
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-07-13 16:19:35 -0400

    Improve crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,10 @@
 //!
 //! # Examples
 //!
+//! In computer chess, one popular way of representing the occupants of a board
+//! is through a [`Bitboard`][bitboard] type. In this type, each individual bit
+//! is a square on a chess board.
+//!
 //! ```
 //! #[macro_use]
 //! extern crate bit_collection;
@@ -26,14 +30,53 @@
 //! #[derive(Copy, Clone)]
 //! pub struct Square(u8);
 //!
+//! /// A set of sixty-four `Square`s.
 //! #[bit_type = "Square"] // Type used to represent individual bits
-//! #[bit_mask = "!0"]     // Mask used to represent all bits set
+//! #[bit_mask = "!0"]     // Mask representing all bits set (this is the default)
 //! #[bit_retr = "0"]      // Retriever for inner value of the bit type
 //! #[derive(BitCollection)]
 //! pub struct Bitboard(u64);
 //!
 //! # fn main() {}
 //! ```
+//!
+//! We can also represent castle rights this way.
+//!
+//! ```
+//! # #[macro_use]
+//! # extern crate bit_collection;
+//! # use bit_collection::BitCollection;
+//! #[derive(Copy, Clone)]
+//! pub enum CastleRight {
+//!     WhiteKingside,
+//!     BlackKingside,
+//!     WhiteQueenside,
+//!     BlackQueenside,
+//! }
+//!
+//! /// A set of `CastleRight`s.
+//! #[bit_type = "CastleRight"]
+//! #[bit_mask = "0b1111"]
+//! #[derive(BitCollection)]
+//! pub struct CastleRights {
+//!     bits: u8
+//! }
+//!
+//! fn iterate_over(rights: CastleRights) {
+//!     for right in rights {
+//!         match right {
+//!             CastleRight::WhiteKingside  => { /* ... */ },
+//!             CastleRight::BlackKingside  => { /* ... */ },
+//!             CastleRight::WhiteQueenside => { /* ... */ },
+//!             CastleRight::BlackQueenside => { /* ... */ },
+//!         }
+//!     }
+//! }
+//!
+//! # fn main() {}
+//! ```
+//!
+//! [bitboard]: https://chessprogramming.wikispaces.com/Bitboards
 
 // Reexport derive macro.
 #[allow(unused_imports)]
