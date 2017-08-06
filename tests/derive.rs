232d108de7cfa0e17b5f60ab814fa3011e7d8949
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-08-06 15:29:41 -0400

    [Tests] Move macros to 'derive.rs'

--- tests/derive.rs
+++ tests/derive.rs
@@ -4,8 +4,24 @@ extern crate bit_collection;
 use std::fmt::Debug;
 use bit_collection::BitCollection;
 
-#[macro_use]
-mod macros;
+macro_rules! count {
+    () => { 0 };
+    ($x:tt) => { 1 };
+    ($x:tt $y:tt $z:tt $w:tt $($rest:tt)*) => { 4 + count!($($rest)*) };
+}
+
+macro_rules! enum_impl {
+    ($(#[$attr:meta])* enum $ident:ident { $($x:ident),* $(,)* }) => {
+        $(#[$attr])*
+        enum $ident {
+            $($x),*
+        }
+
+        impl $ident {
+            fn all() -> [$ident; count!($($x)*)] { [$($ident::$x),*] }
+        }
+    }
+}
 
 enum_impl! {
     #[derive(Copy, Clone, Debug, Eq, PartialEq)]
