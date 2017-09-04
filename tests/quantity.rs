bb7cecf55ea7674aa034186ee557c99bf6d03225
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-09-04 09:12:44 -0400

    [Tests] Use u8::MAX for quantity test

--- tests/quantity.rs
+++ tests/quantity.rs
@@ -1,7 +1,12 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+#[cfg(feature = "std")]
+extern crate core;
+
 #[macro_use]
 extern crate bit_collection;
+
+use core::u8;
 use bit_collection::*;
 
 #[derive(Copy, Clone)]
@@ -13,7 +18,7 @@ struct U8Bits(u8);
 
 #[test]
 fn quantity() {
-    for b in (0..0xFF).map(|x| U8Bits(x)) {
+    for b in (0..u8::MAX).map(|x| U8Bits(x)) {
         let q = match b.len() {
             0 => Quantity::None,
             1 => Quantity::Single,
